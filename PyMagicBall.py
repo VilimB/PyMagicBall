de911059ec23995706c8c48307f17bcf434d2cdc
Wojciech Bojewski <wojciech.bojewski@gmail.com>
2017-12-10 11:23:59 +0100

    PyMagicBall v1.0
    PyGame sample

---
+++ PyMagicBall.py
@@ -0,0 +1,31 @@
+﻿import pygame, sys, random, time
+from pygame.locals import *
+
+answers = ['Yes','No','Maybe','Ask later']
+
+pygame.init()
+SCREEN = pygame.display.set_mode((600, 600))
+pygame.display.set_caption('Magic Ball')
+ball = pygame.image.load("Magic crystal ball.png")
+
+font = pygame.font.SysFont("verdana", 30)
+SCREEN.blit(ball,(0,0))
+text = font.render('CLICK', True, (255, 255, 255))
+SCREEN.blit(text,(260,280))
+
+while True: # main game loop
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit(0)
+        if event.type == pygame.MOUSEBUTTONUP:
+            SCREEN.blit(ball,(0,0))
+            word = answers[random.choice(range(len(answers)))]
+            text = font.render(word, True, (random.choice((0,255)), random.choice((0,255)), random.choice((0,255))))
+            SCREEN.blit(text,(300-len(word)*8,280))
+            pygame.display.update()
+            time.sleep(2)
+            SCREEN.blit(ball,(0,0))
+            text = font.render('CLICK', True, (255, 255, 255))
+            SCREEN.blit(text,(260,280))
+    pygame.display.update()
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PyMagicBall
 You don't have to choose, just ask!
+
+# Resources
+
+The picture was taken from clker.com (http://www.clker.com/clipart-79105.html)
